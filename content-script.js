f0ad40fc5b9b98c181664058052b06b5078321e5
Kyrylo Smolov <smolov326@gmail.com>
2024-01-18 17:25:58 +0200

    mvp

--- content-script.js
+++ content-script.js
@@ -1,44 +1,75 @@
+let running = false;
+let bg;
+
 function timeout(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+let savedTweets = []
+function findTweet(all) {
+    for (let tweet of all) {
+        let tweetText = tweet.querySelector("[data-testid='tweetText']").innerText
+        if (!tweetText || tweetText == "")
+            return null;
+        if (!savedTweets.find(s => s.includes(tweetText))) {
+            savedTweets.push(tweetText)
+            // check if yours
+            let profileLink = tweet.querySelector("[data-testid='User-Name']>div>div>a").href;
+            tweet.style.background = "#D3D3D3";
+            //kuku
+            if (!profileLink.includes("smolov_k_")) 
+                return tweet
+        }
+    }
+}
+
 chrome.runtime.onMessage.addListener(
     async (message, sender, sendResponse) => {
-            sendResponse("ok");
+            sendResponse(!running);
+            if (running) {
+                running = false
+                document.querySelector("[data-testid='SideNav_AccountSwitcher_Button']").style.background = bg;
+                return;
+            }
+            bg = document.querySelector("[data-testid='SideNav_AccountSwitcher_Button']").style.background
+            document.querySelector("[data-testid='SideNav_AccountSwitcher_Button']").style.background = "green";
+            running = true;
             // TODO dont forget to check if text exists
-            let tweets = document.querySelectorAll("[data-testid='tweet']")
-            for (let i in tweets) {
-                timeout(1000);
-                let tweet = document.querySelectorAll("[data-testid='tweet']")[i]
-                // console.log(tweets)
-                // tweets[tweets.length-1].scrollIntoView();
-                // // tweets[3].click()
-                // tweets = document.querySelectorAll("[data-testid='tweet']")
-                // console.log(tweets)
-                let tweetText = tweet
-                                    .children[0]
-                                    .children[0]
-                                    .children[1]
-                                    .children[1]
-                                    .children[1]
-                                    .children[0]
-                                    .innerText
-                if (!tweetText)
+            let tweets = []
+            let tweet;
+            for (;;) {
+                if (!running)
+                    break;
+                await timeout(1000);
+                tweets = document.querySelectorAll("[data-testid='tweet']")
+
+                tweet = findTweet(tweets);
+                if (!tweet)
                     continue;
+                let tweetText = tweet.querySelector("[data-testid='tweetText']").innerText
+
+                await timeout(300)
                 await tweet.scrollIntoView();
-                if (tweetText.includes("")) {
+                await timeout(3000)
+                if (tweetText.toLowerCase().includes("gm")) {
                     const likeButton = tweet.querySelector("[data-testid='like']")
-                    likeButton.click()
+                    if (likeButton)
+                        likeButton.click()
+                    await timeout(100)
                     tweet.click()
                     await timeout(2000)
-                    // let input = document.querySelector('[data-text="true"]').parentElement
-                    // input.textContent = "gm"
-                    // input.click()
-                    // input.dispatchEvent(new Event("input", {bubbles: true}))
+                    let input = document.querySelector('[data-text="true"]').parentElement
+                    //kuku
+                    let array = ["gm mate!", "gm bro", "GM!"]
+                    let reply = array[Math.floor(Math.random() * array.length)];
+                    input.textContent = reply;
+                    input.click()
+                    input.dispatchEvent(new Event("input", {bubbles: true}))
 
                     let button = document.querySelector("[data-testid='tweetButtonInline']")
                     button.click()
-                    timeout(300)
+                    console.log("replied!", reply)
+                    timeout(1000)
                     document.querySelector("[data-testid='app-bar-back']").click()
                     timeout(300)
                 }
--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,10 @@
+let running = false;
 
-console.log(123)
-// https://stackoverflow.com/questions/45179138/sending-message-from-popup-to-content-script-chrome-extension
-test.addEventListener("click", () => {
+run.addEventListener("click", () => {
     chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, {msg:"start"}, (response) => {window.close();});
+        chrome.tabs.sendMessage(tabs[0].id, {msg:"start"}, (response) => {
+            console.log(response)
+            window.close();
+        });
     });
 });
